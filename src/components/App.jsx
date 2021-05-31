31130e6fa55ab95c0ee6c0deae02ae35c8316218
mizuki-yamato <mizuki.yamato@gmail.com>
2021-05-31 22:09:49 +0900

    1 file modified, 2 added, 1 deleted

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ColorfulMessage from './ColorfulMessage';
+
+const App = () => {
+  return <ColorfulMessage color="red" message="こんにちは！" />;
+};
+
+export default App;
