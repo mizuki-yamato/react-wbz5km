a77d7850021811e6dd03daf498eff424db2b9a20
mizuki-yamato <mizuki.yamato@gmail.com>
2021-05-31 22:26:32 +0900

    2 files modified

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ColorfulMessage from './ColorfulMessage';
 
 const App = () => {
-  return <ColorfulMessage color="red" message="こんにちは！" />;
+  const onclickCountUp = () => {
+    setNum(num+1);
+  };
+  const [num, setNum] = useState(0);
+  return (
+    <>
+  <ColorfulMessage color="red" message="こんにちは！">やっほー</ColorfulMessage>
+  <button onClick={onclickCountUp}>カウントアップ</button>
+  <p>{num}</p>
+  </>);
 };
 
 export default App;
